faabb6672056e0b4c6e6ec3f3f707b441d4bd9c9
Nicholas Boshart <nichabosh.code@gmail.com>
2021-03-25 19:30:58 -0400

    fix: project endpoint modifications
    * converted routes in 58c498a to GET, POST, and PATCH requests
       - fixed issues regarding http conventions (ref: https://bit.ly/2PK7T06)
       - resolved bug relating to numeric query params in GET request
    * reverted previous prisma migration (i.e. unique_project_name)
       - created revert_unique_name migration in the process

--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -4,9 +4,15 @@ import express from "express";
 export const projectRoutes = express.Router();
 const prisma = new PrismaClient();
 
-projectRoutes.route("/find").get(async (req, res) => {
+projectRoutes.route("/").get(async (req, res) => {
   try {
-    const projects = await prisma.project.findMany({ where: req.body.query });
+    // todo: find better way of retrieving numeric fields
+    ["id", "expo", "round", "table"].forEach(key => {
+      if (req.query.hasOwnProperty(key))
+        // @ts-ignore
+        req.query[key] = parseInt(req.query[key]);
+    });
+    const projects = await prisma.project.findMany({ where: req.query });
     res.status(200).send(projects);
   } catch (err) {
     console.log(`[ERROR] ${err.message}`);
@@ -14,14 +20,14 @@ projectRoutes.route("/find").get(async (req, res) => {
   }
 });
 
-projectRoutes.route("/create").post(async (req, res) => {
+projectRoutes.route("/").post(async (req, res) => {
   try {
     // todo: find better way of retreiving required fields
     const valid = ["name", "description", "githubUrl"]
-      .every(field => req.body.data.hasOwnProperty(field));
+      .every(field => req.body.hasOwnProperty(field));
     if (valid) {
-      const project = await prisma.project.create({ data: req.body.data });
-      res.status(200).send(project);
+      const created = await prisma.project.create({ data: req.body });
+      res.status(201).send(created);
     } else {
       res.status(400).send("Missing 'name', 'description', or 'githubUrl' fields.");
     }
@@ -31,13 +37,13 @@ projectRoutes.route("/create").post(async (req, res) => {
   }
 });
 
-projectRoutes.route("/update").post(async (req, res) => {
+projectRoutes.route("/:id").patch(async (req, res) => {
   try {
-    const count = await prisma.project.updateMany({
-      where: req.body.query,
-      data: req.body.update
+    const updated = await prisma.project.update({
+      where: { id: parseInt(req.params.id) },
+      data: req.body
     });
-    res.status(200).send(count);
+    res.status(200).send(updated);
   } catch (err) {
     console.log(`[ERROR] ${err.message}`);
     res.status(500).send({ error: true, message: err.message });
