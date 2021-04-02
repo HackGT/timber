d80c1ecc7d0184b7127763939907803ddac99e53
Nicholas Boshart <nichabosh.code@gmail.com>
2021-04-02 12:50:28 -0500

    fix: more changes requested in pr (#2)
    * migrated /projects/action/move endpoint to /projects/batch/update
    * manually linted files -- might save some time to use pre-commit hooks
    * removed unnecessary typing (e.g. projects, req/res, etc)

--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -1,53 +1,52 @@
-import { Request, Response } from "express";
-import { Project } from ".prisma/client";
+import express from "express";
 
 import { asyncHandler } from "../utils/asyncHandler";
 import { prisma } from "../common";
-import express from "express";
 
 export const projectRoutes = express.Router();
 
 projectRoutes.route("/").get(
-  asyncHandler(async (req: Request | any, res: Response) => {
+  asyncHandler(async (req: any, res) => {
     // note: query params default to strings (e.g. 1 → '1'),
     // thus numeric fields must be converted to numbers in
     // order for prisma's queries to function as expected
     Object.keys(req.query).forEach(key => {
-      if (["expo", "round", "table"].includes(key))
+      if (["expo", "round", "table"].includes(key)) {
         req.query[key] = parseInt(req.query[key]);
+      }
     });
-    const matches: Project[] = await prisma.project.findMany({
-      where: req.query
+    const matches = await prisma.project.findMany({
+      where: req.query,
     });
-    res.status(200).send(matches);
+    res.status(200).json(matches);
   })
 );
 
 projectRoutes.route("/").post(
-  asyncHandler(async (req: Request, res: Response) => {
-    const created: Project = await prisma.project.create({
-      data: req.body
+  asyncHandler(async (req, res) => {
+    const created = await prisma.project.create({
+      data: req.body,
     });
     res.status(201).json(created);
   })
 );
 
-projectRoutes.route("/action/move").post(
-  asyncHandler(async (req: Request, res: Response) => {
+projectRoutes.route("/batch/update").post(
+  asyncHandler(async (req, res) => {
     const { ids, ...updates } = req.body;
-    const batchPayload: object = await prisma.project.updateMany({
-      where: { id: { in: ids }},
-      data: updates
+    const batchPayload = await prisma.project.updateMany({
+      where: { id: { in: ids } },
+      data: updates,
     });
     res.status(200).json(batchPayload);
   })
 );
 
 projectRoutes.route("/:id").patch(
-  asyncHandler(async (req: Request, res: Response) => {
-    const updated: Project = await prisma.project.update({
+  asyncHandler(async (req, res) => {
+    const updated = await prisma.project.update({
       where: { id: parseInt(req.params.id) },
-      data: req.body
+      data: req.body,
     });
     res.status(200).json(updated);
   })
