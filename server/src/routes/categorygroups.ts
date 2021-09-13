85c16e10ff8cbdc95659faef727a86172983bffa
mxinburritos <mxinburritos@gmail.com>
2021-09-13 18:59:18 -0400

    Update file casing

--- server/src/routes/categorygroups.ts
+++ server/src/routes/categorygroups.ts
@@ -29,6 +29,7 @@ categoryGroupRoutes.route("/").get(
 
 categoryGroupRoutes.route("/").post(
   asyncHandler(async (req, res) => {
+    console.log(req.body);
     const createdCategoryGroup = await prisma.categoryGroup.create({
       data: req.body,
     });
