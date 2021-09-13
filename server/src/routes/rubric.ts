3726854d5d25998eb0b1b8aa56094cb3acb45eb3
Aayush Seth <aayushseth8@gmail.com>
2021-09-13 18:24:31 -0400

    Create rubrics.ts file

---
+++ server/src/routes/rubric.ts
@@ -0,0 +1,10 @@
+// route to get all rubrics for a specific criteria
+criteriaRoutes.route("/").get(asyncHandler(async (req, res) => {}));
+
+// route to add a rubric
+
+criteriaRoutes.route("/").post(asyncHandler(async (req, res) => {}));
+// route to edit a rubric
+criteriaRoutes.route("/").patch(asyncHandler(async (req, res) => {}));
+// route to delete a rubric
+criteriaRoutes.route("/").delete(asyncHandler(async (req, res) => {}));
