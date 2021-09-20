c440ea203420a9458a7ebad0b8f7485d54f8331a
Aayush Seth <aayushseth8@gmail.com>
2021-09-19 20:04:34 -0400

    Complete all rubric routes

--- server/src/routes/rubric.ts
+++ server/src/routes/rubric.ts
@@ -3,17 +3,17 @@ import express from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { prisma } from "../common";
 
-export const criteriaRoutes = express.Router();
+export const rubricRoutes = express.Router();
 
 // route to get all rubrics for a specific criteria
-criteriaRoutes.route("/").get(
+rubricRoutes.route("/").get(
   asyncHandler(async (req, res) => {
     const { criteria } = req.query;
     const filter: any = {};
 
     if (criteria !== undefined) {
-      const rubric: number = parseInt(criteria as string);
-      filter.categoryId = rubric;
+      const criteriaId: number = parseInt(criteria as string);
+      filter.criteriaId = criteriaId;
     }
 
     const rubrics = await prisma.criteria.findMany({ where: filter });
@@ -22,10 +22,42 @@ criteriaRoutes.route("/").get(
 );
 
 // route to add a rubric
-criteriaRoutes.route("/:id").post(asyncHandler(async (req, res) => {}));
+rubricRoutes.route("/create").post(
+  asyncHandler(async (req, res) => {
+    const createdRubric = await prisma.rubric.createMany({
+      data: req.body.data,
+    });
+    res.status(201).json(createdRubric);
+  })
+);
 
 // route to edit a rubric
-criteriaRoutes.route("/:id").patch(asyncHandler(async (req, res) => {}));
+rubricRoutes.route("/:id").patch(
+  asyncHandler(async (req, res) => {
+    const rubricId: number = parseInt(req.params.id);
+
+    const updatedRubric = await prisma.criteria.update({
+      where: {
+        id: rubricId,
+      },
+      data: req.body,
+    });
+
+    res.status(200).json(updatedRubric);
+  })
+);
 
 // route to delete a rubric
-criteriaRoutes.route("/:id").delete(asyncHandler(async (req, res) => {}));
+rubricRoutes.route("/:id").delete(
+  asyncHandler(async (req, res) => {
+    const rubricId: number = parseInt(req.params.id);
+
+    const deletedRubric = await prisma.criteria.delete({
+      where: {
+        id: rubricId,
+      },
+    });
+
+    res.status(204).end();
+  })
+);
