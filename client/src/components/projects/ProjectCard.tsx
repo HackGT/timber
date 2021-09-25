a0302476eb71ce6613632455f1d9466f0af25256
Aayush Seth <aayushseth8@gmail.com>
2021-09-25 12:55:48 -0400

    Use antd Tag instead of Button

--- client/src/components/projects/ProjectCard.tsx
+++ client/src/components/projects/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card } from "antd";
+import { Card, Tag } from "antd";
 
 import { Project } from "../../types/Project";
 
@@ -12,7 +12,7 @@ const ProjectCard: React.FC<Props> = props => (
     <Card title={props.project.name}>
       <p>Expo: #{props.project.expo}</p>
       <p>Table: {props.project.table}</p>
-      <Button>Project Category</Button>
+      <Tag>Project Category</Tag>
     </Card>
   </>
 );
