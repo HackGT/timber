48439fcd33714586cc60c7daf9356a4b5d36a1f8
Aayush Seth <aayushseth8@gmail.com>
2021-09-25 13:35:37 -0400

    Include query for categories in GET

--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -18,6 +18,9 @@ projectRoutes.route("/").get(
 
     const matches = await prisma.project.findMany({
       where: filter,
+      include: {
+        categories: true,
+      },
     });
     res.status(200).json(matches);
   })
