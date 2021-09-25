c51e88cc966fe7c965d56967cf92cc9997f3f306
Aayush Seth <aayushseth8@gmail.com>
2021-09-25 13:34:40 -0400

    Implement dynamic rendering of project tags

--- client/src/components/projects/ProjectCard.tsx
+++ client/src/components/projects/ProjectCard.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Card, Tag } from "antd";
 
 import { Project } from "../../types/Project";
+import { Category } from "../../types/Category";
 
 interface Props {
+  key: number;
   project: Project;
 }
 
-const ProjectCard: React.FC<Props> = props => (
-  <>
-    <Card title={props.project.name}>
-      <p>Expo: #{props.project.expo}</p>
-      <p>Table: {props.project.table}</p>
-      <Tag>Project Category</Tag>
-    </Card>
-  </>
-);
+const ProjectCard: React.FC<Props> = props => {
+  const tags = props.project.categories.map((category: Category) => category.name);
+
+  return (
+    <>
+      {/* Add key */}
+      <Card key={props.key} title={props.project.name}>
+        <p>Expo: #{props.project.expo}</p>
+        <p>Table: {props.project.table}</p>
+        {tags.map((tag: string) => (
+          <Tag>{tag}</Tag>
+        ))}
+      </Card>
+    </>
+  );
+};
 
 export default ProjectCard;
