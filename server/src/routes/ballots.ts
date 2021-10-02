c1d01f39dfac42a5b8e4f591b7f6ec1716157e5d
mxinburritos <mxinburritos@gmail.com>
2021-10-02 05:32:08 -0400

    Fix get and update endpoints

--- server/src/routes/ballots.ts
+++ server/src/routes/ballots.ts
@@ -1,23 +1,44 @@
 import express from "express";
-import { StatusCodeError } from "request-promise/errors";
 
 import { asyncHandler } from "../utils/asyncHandler";
 import { prisma } from "../common";
-import { criteriaRoutes } from "./criteria";
+
+import { Ballot } from ".prisma/client";
 
 export const ballotsRoutes = express.Router();
 
 ballotsRoutes.route("/").get(
   asyncHandler(async (req, res) => {
-    const { ballot } = req.query;
+    const { criterium } = req.query;
     const filter: any = {};
 
-    if (ballot !== undefined) {
-      const ballotId: number = parseInt(ballot as string);
-      filter.id = ballotId;
+    if (criterium !== undefined) {
+      if (Array.isArray(criterium)) {
+        const ballots: Ballot[] = [];
+        for (let i = 0; i < criterium.length; i++) {
+          // eslint-disable-next-line no-await-in-loop
+          const newBallots = await prisma.ballot.findMany({
+            where: {
+              criteriaId: parseInt(criterium[i] as string),
+              deleted: false,
+            },
+          });
+          console.log(newBallots);
+          ballots.push(...newBallots);
+        }
+
+        res.status(200).json(ballots);
+        return;
+      }
+      const criteriaId: number = parseInt(criterium as string);
+      filter.criteriaId = criteriaId;
     }
 
     filter.deleted = false;
+
+    // const ballots = await prisma.ballot.findMany({
+    //   where: filter,
+    // });
 
     const ballots = await prisma.ballot.findMany({
       where: filter,
@@ -50,14 +71,37 @@ ballotsRoutes.route("/").post(
 ballotsRoutes.route("/:id").patch(
   asyncHandler(async (req, res) => {
     const ballotId: number = parseInt(req.params.id);
+    const { score, userId } = req.body;
 
-    const { score } = req.body;
+    const ballots = await prisma.ballot.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        criteria: true,
+      },
+    });
 
     const ballot = await prisma.ballot.findFirst({
       where: {
         id: ballotId,
       },
+      include: {
+        criteria: true,
+      },
     });
+
+    const filteredBallots = ballots.filter(
+      b => b.criteria.categoryId === ballot?.criteria.categoryId
+    );
+
+    if (filteredBallots.length > 1) {
+      throw new Error("You can only judge one project for each category");
+    }
+
+    if (ballot?.userId !== userId) {
+      throw new Error("You're not authorized to change the score on this ballot.");
+    }
 
     const criteria = await prisma.criteria.findFirst({
       where: {
