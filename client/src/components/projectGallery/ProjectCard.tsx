d9683a0411cb803d6b72038a00d9d476a5021ea4
Aayush Seth <aayushseth8@gmail.com>
2022-02-27 18:50:11 -0500

    Refactor ProjectGalleryCard to use new GET Route

--- client/src/components/projectGallery/ProjectCard.tsx
+++ client/src/components/projectGallery/ProjectCard.tsx
@@ -6,23 +6,32 @@ import { Category } from "../../types/Category";
 import { User } from "../../types/User";
 import { UserRole } from "../../types/UserRole";
 import { TableGroup } from "../../types/TableGroup";
+import useAxios from "axios-hooks";
+import LoadingDisplay from "../../displays/LoadingDisplay";
+import ErrorDisplay from "../../displays/ErrorDisplay";
 
 interface Props {
   key: number;
   project: Project;
   user?: User;
-  tablegroups: TableGroup[];
   onClick?: (data: any) => any;
 }
 
 const ProjectCard: React.FC<Props> = props => {
-  console.log(props.tablegroups);
-  const tablegroup = props.tablegroups.filter(
-    (group: TableGroup) => group.id === props.project.tableGroupId
+  const [{ loading: tablegroupsLoading, data: tablegroupData, error: tablegroupsError }] = useAxios(
+    `/tablegroups/${props.project.tableGroupId}`
   );
+
+  if (tablegroupsLoading) {
+    return <LoadingDisplay />;
+  }
+
+  if (tablegroupsError) {
+    return <ErrorDisplay error={tablegroupsError} />;
+  }
   const tags = props.project.categories.map((category: Category) => category.name);
 
-  console.log(tablegroup);
+  console.log(tablegroupData);
   return (
     <Card
       key={props.key}
@@ -34,9 +43,9 @@ const ProjectCard: React.FC<Props> = props => {
         [UserRole.ADMIN].includes(props.user.role) && <Button onClick={props.onClick}>Edit</Button>
       }
     >
-      <p>TableGroup Name: {tablegroup[0].name}</p>
+      <p>TableGroup Name: {tablegroupData.name}</p>
       <p>Table Number: {props.project.table}</p>
-      <p>TableGroup Color: {tablegroup[0].color}</p>
+      <p>TableGroup Color: {tablegroupData.color}</p>
       <p>Expo: #{props.project.expo}</p>
       <p>
         <a href={props.project.roomUrl} target="_blank" rel="noreferrer">
