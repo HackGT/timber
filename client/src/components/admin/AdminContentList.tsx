d591bb7aa17572a3604207e8e83bd214240477b6
Nimita Deshpande <nimita.ahs@gmail.com>
2022-05-26 12:18:49 -0400

    Added filter by user role, filter by judging, and pagination to the users tab on the admin page

--- client/src/components/admin/AdminContentList.tsx
+++ client/src/components/admin/AdminContentList.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Button, List, Typography, Input } from "antd";
+import { Button, List, Typography, Input, Col, Select, Tag, Dropdown, Checkbox, Pagination, PaginationProps } from "antd";
 import { ListGridType } from "antd/lib/list";
 import useAxios from "axios-hooks";
 
 import { FormModalProps, ModalState } from "../../util/FormModalProps";
 import ErrorDisplay from "../../displays/ErrorDisplay";
 import LoadingDisplay from "../../displays/LoadingDisplay";
+import { User } from "../../types/User";
 
 const { Title } = Typography;
 const { Search } = Input;
@@ -15,8 +16,11 @@ interface Props {
   queryUrl: string;
   sortData: (data: any) => any;
   modal: React.FC<FormModalProps>;
+  showSortUsersByRoleButton?: boolean;
+  showIsJudging?: boolean;
   hideAddButton?: boolean;
   searchFilterField: string;
+  userRoleFilterField: string;
   renderItem: (item: any, index: number, openModal: (values: any) => void) => React.ReactNode;
   listGrid?: ListGridType;
   listBordered?: boolean;
@@ -28,6 +32,8 @@ const AdminContentList: React.FC<Props> = props => {
     initialValues: null,
   } as ModalState);
   const [searchText, setSearchText] = useState("");
+  const [userRole, setUserRole] = useState<any>(undefined);
+  const [isJudging, setIsJudging] = useState<any>(undefined);
 
   const [{ loading, data, error }, refetch] = useAxios(props.queryUrl);
 
@@ -46,13 +52,37 @@ const AdminContentList: React.FC<Props> = props => {
     return <ErrorDisplay error={error} />;
   }
 
-  const updatedData = data
+  let updatedData = data
     ? props
         .sortData(data)
         .filter((item: any) =>
           item[props.searchFilterField].toLowerCase().includes(searchText.toLowerCase())
         )
     : [];
+
+    const userRoleOptions = [
+      { value: 'GENERAL', label: 'General' },
+      { value: 'SPONSOR', label: 'Sponsor' },
+      { value: 'ADMIN', label: 'Admin' }
+    ]
+
+    updatedData = userRole
+      ? updatedData.filter((user: User) =>
+          user.role == userRole
+        )
+      : updatedData;
+
+    const judgingOptions = [
+      { value: 'true', label: 'Is Judging' },
+      { value: 'false', label: 'Is Not Judging' }
+    ]
+
+    updatedData = isJudging
+      ? 
+      updatedData.filter((user: User) =>
+          (user.isJudging).toString() == isJudging 
+        )
+      : updatedData;
 
   const Modal = props.modal;
 
@@ -65,6 +95,26 @@ const AdminContentList: React.FC<Props> = props => {
         value={searchText}
         onChange={event => setSearchText(event.target.value)}
       />
+      { props.showSortUsersByRoleButton && (
+        <Select
+          placeholder="Filter by User Role"
+          style={{ width: "300px" }}
+          optionFilterProp="label1"
+          onChange={(value: any) => setUserRole(value)}
+          options={userRoleOptions}
+          allowClear
+        />
+      )}
+      {props.showSortUsersByRoleButton && (
+        <Select
+          placeholder="Filter by Judging"
+          style={{ width: "300px" }}
+          optionFilterProp="label2"
+          onChange={(value: any) => setIsJudging(value)}
+          options={judgingOptions}
+          allowClear
+        />
+      )}
       {!props.hideAddButton && (
         <Button style={{ marginLeft: "10px" }} onClick={() => openModal(null)}>
           Add +
@@ -77,6 +127,9 @@ const AdminContentList: React.FC<Props> = props => {
         renderItem={(item: any, index: any) => props.renderItem(item, index, openModal)}
         grid={props.listGrid}
         bordered={props.listBordered}
+        pagination={{
+          pageSize: 15
+        }}
       />
       <Modal modalState={modalState} setModalState={setModalState} refetch={refetch} />
     </>
