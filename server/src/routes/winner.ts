d807d1b0315b74d1b8d9869d06042dad2d160220
Aayush Seth <aayushseth8@gmail.com>
2022-09-11 17:42:29 -0400

    Get all winners

--- server/src/routes/winner.ts
+++ server/src/routes/winner.ts
@@ -6,47 +6,55 @@ import { isAdmin } from "../auth/auth";
 
 export const winnerRoutes = express.Router();
 
+// Get all winners
+winnerRoutes.route("/").get(
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const winners = await prisma.winner.findMany({});
+    res.status(200).json(winners);
+  })
+);
+
 // add new winner
 winnerRoutes.route("/").post(
-    isAdmin,
-    asyncHandler(async (req, res) => {
-      const createdWinner = await prisma.winner.create({
-        data: req.body.data,
-      });
-      res.status(201).json(createdWinner);
-    })
-  );
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const createdWinner = await prisma.winner.create({
+      data: req.body.data,
+    });
+    res.status(201).json(createdWinner);
+  })
+);
 
 // edit existing winner
 winnerRoutes.route("/:id").patch(
-    isAdmin,
-    asyncHandler(async (req, res) => {
-      const winnerId: number = parseInt(req.params.id);
-  
-      const updatedWinner = await prisma.winner.update({
-        where: {
-          id: winnerId,
-        },
-        data: req.body,
-      });
-  
-      res.status(200).json(updatedWinner);
-    })
-  );
-  
-  // delete existing winner
-  winnerRoutes.route("/:id").delete(
-    isAdmin,
-    asyncHandler(async (req, res) => {
-      const winnerId: number = parseInt(req.params.id);
-  
-      const deletedWinner = await prisma.winner.delete({
-        where: {
-          id: winnerId,
-        },
-      });
-  
-      res.status(204).end();
-    })
-  );
-  
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const winnerId: number = parseInt(req.params.id);
+
+    const updatedWinner = await prisma.winner.update({
+      where: {
+        id: winnerId,
+      },
+      data: req.body,
+    });
+
+    res.status(200).json(updatedWinner);
+  })
+);
+
+// delete existing winner
+winnerRoutes.route("/:id").delete(
+  isAdmin,
+  asyncHandler(async (req, res) => {
+    const winnerId: number = parseInt(req.params.id);
+
+    const deletedWinner = await prisma.winner.delete({
+      where: {
+        id: winnerId,
+      },
+    });
+
+    res.status(204).end();
+  })
+);
