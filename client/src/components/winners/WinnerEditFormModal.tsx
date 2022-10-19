396262db88f86ff45ac099cd6384e437658f7508
Aayush Seth <aayushseth8@gmail.com>
2022-10-19 11:24:48 -0400

    Add delete button in modal

--- client/src/components/winners/WinnerEditFormModal.tsx
+++ client/src/components/winners/WinnerEditFormModal.tsx
@@ -1,15 +1,37 @@
 import React, { useEffect } from "react";
-import { Col, Form, message, Modal, Row, Select } from "antd";
+import { Button, Col, Form, message, Modal, Popconfirm, Row, Select } from "antd";
 import axios from "axios";
 import { apiUrl, Service } from "@hex-labs/core";
 
 import { FormModalProps } from "../../util/FormModalProps";
+import { handleAxiosError } from "../../util/util";
 
 const { Option } = Select;
 
 const WinnerEditFormModal: React.FC<FormModalProps> = props => {
   const [form] = Form.useForm();
   useEffect(() => form.resetFields(), [form, props.modalState.initialValues]);
+
+  const onDelete = async () => {
+    try {
+      if (props.modalState.initialValues) {
+        axios
+          .delete(apiUrl(Service.EXPO, `/winner/${props.modalState.initialValues.id}`))
+          .then(res => {
+            message.success("Winner successfully deleted", 2);
+            props.setModalState({ visible: false, initialValues: null });
+            props.refetch();
+          })
+          .catch(err => {
+            handleAxiosError(err);
+          });
+      } else {
+        message.error("Winner could not be deleted");
+      }
+    } catch (error) {
+      console.log("Validate Failed:", error);
+    }
+  };
 
   const onSubmit = async () => {
     const hide = message.loading("Loading...", 0);
@@ -50,6 +72,27 @@ const WinnerEditFormModal: React.FC<FormModalProps> = props => {
       onCancel={() => props.setModalState({ visible: false, initialValues: null })}
       onOk={onSubmit}
       bodyStyle={{ paddingBottom: 0 }}
+      footer={[
+        <Popconfirm
+          title="Are you sure you want to delete this Winner?"
+          onConfirm={onDelete}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button danger style={{ float: "left" }}>
+            Delete
+          </Button>
+        </Popconfirm>,
+        <Button
+          key="2"
+          onClick={() => props.setModalState({ visible: false, initialValues: null })}
+        >
+          Cancel
+        </Button>,
+        <Button key="1" onClick={onSubmit} type="primary">
+          Update
+        </Button>,
+      ]}
     >
       <Form
         form={form}
