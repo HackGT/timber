94cc8a7de9e5f6fbeece470133809e8fce866a0c
Ayush Goyal <agoyal2001@gmail.com>
2023-08-03 17:54:43 -0400

    Fix project card styling in gallery

--- src/components/projectGallery/ProjectGallery.tsx
+++ src/components/projectGallery/ProjectGallery.tsx
@@ -141,7 +141,7 @@ const ProjectGallery: React.FC<Props> = props => {
         </Col>
       </Row>
       <List
-        grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 4, xxl: 4 }}
+        grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 5, xxl: 6 }}
         loading={projectsLoading}
         dataSource={updatedData}
         renderItem={(project: Project) => (
